a968fd7e80a0d142859e415c03b4510a92f0e2c6
test231212 <j910219@gmail.com>
2024-03-12 12:47:58 +0900

    게시글 목록보기

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,10 +1,14 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,14 +22,16 @@ public class BoardController {
         return "redirect:/";
     }
 
-    @GetMapping("/")
-    public String index() {
+    @GetMapping("/" )
+    public String index(HttpServletRequest request) {
+        List<Board> boardList = boardNativeRepository.findAll();
+        request.setAttribute("boardList", boardList);
         return "index";
     }
 
     @GetMapping("/board/save-form")
     public String saveForm() {
-        return "/board/save-form";
+        return "board/save-form";
     }
 
     @GetMapping("/board/{id}")
