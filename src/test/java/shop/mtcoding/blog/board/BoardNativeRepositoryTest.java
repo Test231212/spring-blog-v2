021df9fbf90458f6d225b6b42bd681ad9e5d89df
test231212 <j910219@gmail.com>
2024-03-12 14:27:46 +0900

    게시글 상세보기

--- src/test/java/shop/mtcoding/blog/board/BoardNativeRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardNativeRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @Import(BoardNativeRepository.class)
 @DataJpaTest
@@ -15,6 +17,20 @@ public class BoardNativeRepositoryTest {
 
     @Autowired // DI
     private BoardNativeRepository boardNativeRepository;
+
+    @Test
+    public void findById_test(){
+        // given
+        int id = 1;
+
+        // when
+        Board board = boardNativeRepository.findById(id);
+        //System.out.println("findById_test "+board);
+
+        // then
+        assertThat(board.getTitle()).isEqualTo("제목1");
+        assertThat(board.getContent()).isEqualTo("내용1");
+    }
 
     @Test
     public void findAll_test(){
@@ -27,8 +43,7 @@ public class BoardNativeRepositoryTest {
         System.out.println("findAll_test/size : "+boardList.size());
         System.out.println("findAll_test/username : "+boardList.get(2).getUsername());
 
-        // org.assertj.core.api
-        Assertions.assertThat(boardList.size()).isEqualTo(4);
-        Assertions.assertThat(boardList.get(2).getUsername()).isEqualTo("ssar");
+        assertThat(boardList.size()).isEqualTo(4);
+        assertThat(boardList.get(2).getUsername()).isEqualTo("ssar");
     }
 }
