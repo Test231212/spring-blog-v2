0042cb0d97810aa4941f6abc1249c6ea8c15c08c
test231212 <j910219@gmail.com>
2024-03-14 17:44:12 +0900

    게시글 쓰기 완료

--- src/main/java/shop/mtcoding/blog/board/BoardRequest.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRequest.java
@@ -1,5 +1,22 @@
 package shop.mtcoding.blog.board;
 
+import lombok.Data;
+import shop.mtcoding.blog.user.User;
+
 public class BoardRequest {
 
+    @Data
+    public static class SaveDTO{
+        private String title;
+        private String content;
+
+        // DTO를 client로부터 받아서 PersistContext에 전달하기 위해 사용
+        public Board toEntity(User user){
+            return Board.builder()
+                    .title(title)
+                    .content(content)
+                    .user(user)
+                    .build();
+        }
+    }
 }
