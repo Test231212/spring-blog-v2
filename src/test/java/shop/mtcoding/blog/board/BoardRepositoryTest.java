535dcf97eaa04fc0d5a4b2dabdbc70272f3af2f0
test231212 <j910219@gmail.com>
2024-03-14 12:35:36 +0900

    게시글 목록보기

--- src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -5,12 +5,74 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
 @Import(BoardRepository.class)
 @DataJpaTest
 public class BoardRepositoryTest {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Test
+    public void findAllV2_test(){
+        List<Board> boardList = boardRepository.findAllV2();
+        System.out.println("findAllV2_test : 조회완료 쿼리 2번");
+        boardList.forEach(board -> {
+            System.out.println(board);
+        });
+    }
+
+    @Test
+    public void randomquery_test(){
+        int[] ids = {1,2};
+
+        // select u from User u where u.id in (?,?);
+        String q = "select u from User u where u.id in (";
+        for (int i=0; i<ids.length; i++){
+            if(i==ids.length-1){
+                q = q + "?)";
+            }else{
+                q = q + "?,";
+            }
+        }
+        System.out.println(q);
+    }
+
+    @Test
+    public void findAll_custom_inquery_test() {
+        List<Board> boardList = boardRepository.findAll();
+
+        int[] userIds = boardList.stream().mapToInt(board -> board.getUser().getId()).distinct().toArray();
+        for (int i : userIds){
+            System.out.println(i);
+        }
+
+        // select * from user_tb where id in (3,2,1,1);
+        // select * from user_tb where id in (3,2,1);
+    }
+
+    @Test
+    public void findAll_lazyloading_test() {
+        List<Board> boardList = boardRepository.findAll();
+        boardList.forEach(board -> {
+            System.out.println(board.getUser().getUsername());
+        });
+
+    }
+
+    @Test
+    public void findAll_test() {
+        // given
+
+        // when
+        List<Board> boardList = boardRepository.findAll();
+        boardList.forEach(board -> {
+            System.out.println(board.getUser().getUsername());
+        });
+
+        // then
+    }
 
     @Test
     public void findByIdJoinUser_test(){
