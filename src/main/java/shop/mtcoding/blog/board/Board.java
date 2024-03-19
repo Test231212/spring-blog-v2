844922ad45cc3672787f1000bf4dc9e922a7dec5
test231212 <j910219@gmail.com>
2024-03-19 17:42:52 +0900

    머지 테스트

--- src/main/java/shop/mtcoding/blog/board/Board.java
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -1,12 +1,15 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import shop.mtcoding.blog.util.MyDateUtil;
 
 import java.sql.Timestamp;
 
+@NoArgsConstructor
 @Data
 @Table(name = "board_tb")
 @Entity
@@ -20,7 +23,16 @@ public class Board {
     private String username;
     private Timestamp createdAt;
 
-    public String getTime(){
+    @Builder
+    public Board(Integer id, String title, String content, String username, Timestamp createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.username = username;
+        this.createdAt = createdAt;
+    }
+
+    public String getTime() {
         return MyDateUtil.timestampFormat(createdAt);
     }
 }
