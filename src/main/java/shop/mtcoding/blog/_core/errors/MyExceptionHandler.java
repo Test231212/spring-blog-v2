17a9083dd9db3623fb1a71fd391c699593470cc0
test231212 <j910219@gmail.com>
2024-03-20 14:54:51 +0900

    RestAPI요청과 응답 세팅

--- src/main/java/shop/mtcoding/blog/_core/errors/MyExceptionHandler.java
+++ src/main/java/shop/mtcoding/blog/_core/errors/MyExceptionHandler.java
@@ -1,9 +1,7 @@
 package shop.mtcoding.blog._core.errors;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import shop.mtcoding.blog._core.errors.exception.*;
@@ -18,25 +16,28 @@ public class MyExceptionHandler {
         ApiUtil<?> apiUtil = new ApiUtil<>(400, e.getMessage()); // http body -> 구성한 객체
         return new ResponseEntity<>(apiUtil, HttpStatus.BAD_REQUEST); // http body, http header
     }
+
     @ExceptionHandler(Exception401.class)
     public ResponseEntity<?> ex401(Exception401 e){
-        ApiUtil<?> apiUtil = new ApiUtil<>(401, e.getMessage()); // http body -> 구성한 객체
-        return new ResponseEntity<>(apiUtil, HttpStatus.UNAUTHORIZED); // http body, http header
-    }
-    @ExceptionHandler(Exception403.class)
-    public ResponseEntity<?> ex401(Exception403 e){
-        ApiUtil<?> apiUtil = new ApiUtil<>(403, e.getMessage()); // http body -> 구성한 객체
-        return new ResponseEntity<>(apiUtil, HttpStatus.FORBIDDEN); // http body, http header
-    }
-    @ExceptionHandler(Exception404.class)
-    public ResponseEntity<?> ex404(Exception404 e){
-        ApiUtil<?> apiUtil = new ApiUtil<>(404, e.getMessage()); // http body -> 구성한 객체
-        return new ResponseEntity<>(apiUtil, HttpStatus.NOT_FOUND); // http body, http header
-    }
-    @ExceptionHandler(Exception500.class)
-    public ResponseEntity<?> ex500(Exception500 e){
-        ApiUtil<?> apiUtil = new ApiUtil<>(500, e.getMessage()); // http body -> 구성한 객체
-        return new ResponseEntity<>(apiUtil, HttpStatus.INTERNAL_SERVER_ERROR); // http body, http header
+        ApiUtil<?> apiUtil = new ApiUtil<>(401, e.getMessage());
+        return new ResponseEntity<>(apiUtil, HttpStatus.UNAUTHORIZED);
     }
 
+    @ExceptionHandler(Exception403.class)
+    public ResponseEntity<?> ex403(Exception403 e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(403, e.getMessage());
+        return new ResponseEntity<>(apiUtil, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(Exception404.class)
+    public ResponseEntity<?> ex404(Exception404 e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(404, e.getMessage());
+        return new ResponseEntity<>(apiUtil, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception500.class)
+    public ResponseEntity<?> ex500(Exception500 e){
+        ApiUtil<?> apiUtil = new ApiUtil<>(500, e.getMessage());
+        return new ResponseEntity<>(apiUtil, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
